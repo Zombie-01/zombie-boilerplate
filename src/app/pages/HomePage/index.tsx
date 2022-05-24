1208382208b23aa392cdb2d9efc01a5f65eb29ea
Zombie-01 <mm6816557@gmail.com>
2022-05-24 15:21:36 +0800

    s

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,13 +1,10 @@
 import * as React from 'react';
 import { NavBar } from 'app/components/NavBar';
-import { Container } from '@chakra-ui/react';
 
 export function homepage() {
   return (
     <>
-      <Container maxW="1200px" height="100vh">
-        <NavBar />
-      </Container>
+      <NavBar />
     </>
   );
 }
